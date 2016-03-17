eecddf5de00d8deeda5f9f231a68b0c38da670f4
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-03-17 13:14:27 +0100

    modify build structure

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/21 21:06:49 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/03/09 15:49:38 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/03/17 13:14:01 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "libft.h"
+#include <libft.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
