ddc7f98271cd17b93ef7f354b638c82b6bfcba4f
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-03-23 08:05:58 +0100

    add rendu script

---
+++ util/rendu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+GITHUB_URL=$(git remote -v | grep push | awk '{print $2}')
+PROJECT_NAME=$(echo $GITHUB_URL | awk -F/ '{print $NF}' | cut -d. -f1)
+if [ -x $PROJECT_DIR ]; then
+	PROJECT_DIR=/tmp/$PROJECT_NAME
+fi
+RENDU_URL=$1
+
+echo NAME: $PROJECT_NAME
+echo DIR: $PROJECT_DIR
+
+if [ -n "$RENDU_URL" ]; then
+	git clone $42_URL /tmp
+else
+	if [ -d "$PROJECT_DIR" ]; then
+		rm -rf $PROJECT_DIR
+	fi
+	mkdir $PROJECT_DIR
+fi
+
+CPY=(get_next_line.*)
+
+for DIR in ${CPY[@]}; do
+	cp -r $DIR $PROJECT_DIR
+done
+
+make -C $PROJECT_DIR all fclean
+find $PROJECT_DIR -mindepth 2 -type d -name ".git" -exec rm -rf {} \; 2>/dev/null
+
+exit 0
