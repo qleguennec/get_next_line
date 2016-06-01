eea9f698ec65270447befaa312c9995fd400f048
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-06-01 03:14:09 +0200

    rebuild

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/21 21:06:49 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/06/01 01:43:50 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/06/01 03:08:43 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,10 @@
 # include <stdlib.h>
 #include <stdio.h>
 
-# define BUFF_SIZE	1
-# define SEP_CHAR	'\n'
-# define RL			g_stat->read
-# define LR			((t_read *)g_stat->read->content)->str
-# define LR_LEN		((t_read *)g_stat->read->content)->slen
-# define LR_RET		((t_read *)g_stat->read->content)->r_return
+# define BUFF_SIZE 8
+# define SEP_CHAR '\n'
 
-typedef struct	s_status
-{
-	t_list		*read;
-	char		*line_end;
-	size_t		slen;
-	int			end;
-}				t_status;
-
-typedef struct	s_read
-{
-	char		str[BUFF_SIZE];
-	size_t		slen;
-	int			r_return;
-}				t_read;
-
-static t_status	*g_stat = NULL;
+static t_list	*g_stat = NULL;
 
 int				get_next_line(int fd, char **line);
 
--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/21 21:05:00 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/06/01 01:44:37 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/06/01 03:14:08 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,94 +14,41 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-static int	do_read
-	(int fd)
+int			get_next_line(int fd, char **line)
 {
 	t_list	*l;
-	t_read	r;
-
-	r.r_return = read(fd, r.str, BUFF_SIZE);
-	if (r.r_return < 0)
-		return (-1);
-	if (!r.r_return)
-	{
-		g_stat->line_end = LR + LR_LEN;
-		return (0);
-	}
-	if (!(l = ft_lstnew(&r, sizeof(r))))
-		return (-1);
-	ft_lstadd(&RL, l);
-	LR_LEN = LR_RET;
-	g_stat->line_end = ft_memchr(LR, SEP_CHAR, LR_LEN);
-	if (!g_stat->line_end && LR_LEN < BUFF_SIZE)
-		LR[LR_LEN++] = SEP_CHAR;
-	g_stat->slen += LR_LEN;
-	return (1);
-}
-
-static int	copy
-	(char **line)
-{
+	char	buffer[BUFF_SIZE];
 	char	*buf;
-	t_list	*l;
+	char	*tmp;
+	int		ret;
 
-	if (!(*line = ft_strnew(g_stat->slen)))
-		return (0);
-	buf = *line + g_stat->slen - (g_stat->line_end - LR);
-	ft_memcpy(buf, LR, g_stat->line_end - LR);
-	l = RL;
-	RL = RL->next;
-	while (RL)
+	if (g_stat)
 	{
-		buf -= LR_LEN;
-		ft_memcpy(buf, LR, LR_LEN);
-		RL = RL->next;
+		tmp = ft_memchr(g_stat->content, SEP_CHAR, g_stat->content_size);
+		ret = g_stat->content_size;
+		printf("(%d)\n", ret);
+		buf = g_stat->content;
 	}
-	RL = l;
-	return (1);
-}
-
-static int	end
-	(char **line, int ret)
-{
-	ft_lstdel(&RL, &ft_delete);
-	if (ret != 1)
+	if (!tmp)
 	{
-		*line = NULL;
+		ret = read(fd, buffer, BUFF_SIZE);
+		buf = buffer;
+		if (!ret)
+			return (0);
+		tmp = ft_memchr(buf, SEP_CHAR, ret);
+	}
+	if (tmp)
+	{
+		l = ft_lstnew(buf, tmp - buf);
+		ft_lstadd(&g_stat, l);
+		ft_lstrevbuild(g_stat);
+		*line = ft_strnew(g_stat->content_size);
+		ft_memcpy(*line, g_stat->content, g_stat->content_size);
 		ft_memdel((void **)&g_stat);
+		g_stat = ft_lstnew(tmp + 1, (size_t)ret - (tmp + 1 - buf));
+		return (1);
 	}
-	return (ret);
-}
-
-int			get_next_line
-	(int fd, char **line)
-{
-	int		r_ret;
-
-	if (BUFF_SIZE <= 0 || !line)
-		return (-1);
-	if (!g_stat && !(g_stat = ft_memalloc(sizeof(*g_stat))))
-		return (-1);
-	if (g_stat->end)
-		return (end(line, 0));
-	if (!RL && !(RL = ft_lstnew(NULL, sizeof(t_read))))
-		return (-1);
-	g_stat->line_end = ft_memchr(LR, SEP_CHAR, LR_LEN);
-	if (!g_stat->line_end)
-		r_ret = do_read(fd);
-	if (r_ret == -1)
-		return (-1);
-	if (!r_ret)
-		g_stat->end = 1;
-	if (!g_stat->line_end)
-		return (get_next_line(fd, line));
-	LR_LEN -= g_stat->line_end - LR + 1;
-	g_stat->slen -= LR_LEN + 1;
-	if (!copy(line))
-		return (end(line, -1));
-	g_stat->slen = LR_LEN;
-	ft_memmove(LR, g_stat->line_end + 1, LR_LEN);
-	ft_bzero(LR + LR_LEN, BUFF_SIZE - LR_LEN);
-	ft_lstdel(&RL->next, &ft_delete);
-	return (1);
+	l = ft_lstnew(buf, ret);
+	ft_lstadd(&g_stat, l);
+	return (get_next_line(fd, line));
 }
