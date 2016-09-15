2051f363d2cdc422f71c526d65c84a774a772af8
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-09-15 14:10:36 +0200

    fix norm

--- libgnl.h
+++ libgnl.h
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   libgnl.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 08:40:07 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/07/08 11:45:21 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/09/15 14:10:35 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef LIBGNL_H
+# define LIBGNL_H
 
 # include "../libvect/libvect.h"
 
-# define BUFF_SIZE 16
+# define BUFF_SIZE 100
 
 int		get_next_line(int fd, t_vect *v, t_vect *line);
 
