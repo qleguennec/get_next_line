16c3e6947d92509688fdd06e651bcc061d2ec711
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-01-20 13:55:06 +0100

    add gnl_lines, untested

---
+++ gnl_lines.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl_lines.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/20 13:44:24 by qle-guen          #+#    #+#             */
+/*   Updated: 2017/01/20 13:54:09 by qle-guen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include "../libft/malloc.h"
+#include "libgnl.h"
+
+int		gnl_lines(int fd, t_vect *lines, int opts)
+{
+	char	*dup;
+	int		ret;
+	t_vect	buf;
+	t_vect	line;
+
+	vect_init(&buf);
+	vect_init(&line);
+	while ((ret = get_next_line(fd, &buf, &line, opts & GNL_STR)) == 1)
+	{
+		if (!line.used)
+			continue ;
+		MALLOC_N(dup, 1 + line.used);
+		ft_memcpy(dup, line.data, line.used);
+		dup[line.used] = '\0';
+		VECT_ADD(lines, dup);
+		line.used = 0;
+	}
+	free(line.data);
+	free(buf.data);
+	return (ret);
+}
--- libgnl.h
+++ libgnl.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 08:40:07 by qle-guen          #+#    #+#             */
-/*   Updated: 2017/01/16 17:39:29 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/01/20 13:54:42 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@
 # define GNL_CHECK_SIZE		4
 
 int		get_next_line(int fd, t_vect *v, t_vect *line, int opts);
+int		gnl_lines(int fd, t_vect *lines, int opts);
 size_t	gnl_read_all(int fd, t_vect *buf, int opts, size_t *calls);
 
 #endif
